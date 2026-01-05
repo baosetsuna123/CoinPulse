af070891e4225eab344f1b1b9dd4d7b898a9bb1f
baosetsuna <baohse321@gmail.com>
2026-01-05 18:13:31 +0700

    Impl detail coin page

--- lib/coingecko.actions.ts
+++ lib/coingecko.actions.ts
@@ -4,6 +4,7 @@ import qs from 'query-string';
 
 const BASE_URL = process.env.COINGECKO_BASE_URL;
 const API_KEY = process.env.COINGECKO_API_KEY;
+
 if (!BASE_URL) throw new Error('Could not get base url');
 if (!API_KEY) throw new Error('Could not get api key');
 
@@ -35,4 +36,33 @@ export async function fetcher<T>(
   }
 
   return response.json();
+}
+
+export async function getPools(
+  id: string,
+  network?: string | null,
+  contractAddress?: string | null,
+): Promise<PoolData> {
+  const fallback: PoolData = {
+    id: '',
+    address: '',
+    name: '',
+    network: '',
+  };
+
+  if (network && contractAddress) {
+    const poolData = await fetcher<{ data: PoolData[] }>(
+      `/onchain/networks/${network}/tokens/${contractAddress}/pools`,
+    );
+
+    return poolData.data?.[0] ?? fallback;
+  }
+
+  try {
+    const poolData = await fetcher<{ data: PoolData[] }>('/onchain/search/pools', { query: id });
+
+    return poolData.data?.[0] ?? fallback;
+  } catch {
+    return fallback;
+  }
 }
