a7d0c671c609632bc28ba65ae12d95abe087a12a
baosetsuna <baohse321@gmail.com>
2026-01-05 11:23:06 +0700

    Impl navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-const page = () => {
-  return (
-    <div className='text-3xl text-indigo-500 flex justify-center items-center min-h-screen'>CoinPulse</div>
-  )
+const Page = () => {
+    return <div></div>
 }
 
-export default page
+export default Page
